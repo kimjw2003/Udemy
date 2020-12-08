caf805e66c2872a4567094cb8ee542b19933544d
kimjw2003 <kimjw111003@gmail.com>
2020-12-08 23:40:30 +0900

    Corurtines_Basic

--- Coroutines/Coroutines_Basic/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
+++ Coroutines/Coroutines_Basic/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
@@ -16,7 +19,10 @@ class MainActivity : AppCompatActivity() {
             tvCount.text = count++.toString()
         }
         btnDownloadUserData.setOnClickListener {
-            downloadUserData()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
         }
     }
 
