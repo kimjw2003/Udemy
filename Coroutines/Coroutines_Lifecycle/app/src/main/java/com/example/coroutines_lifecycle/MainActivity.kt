75da6822333e9d9d234892b811d9c40bfbfb305d
kimjw2003 <kimjw111003@gmail.com>
2021-01-14 16:37:58 +0900

    [Study] coroutine_lifecycle

--- Coroutines/Coroutines_Lifecycle/app/src/main/java/com/example/coroutines_lifecycle/MainActivity.kt
+++ Coroutines/Coroutines_Lifecycle/app/src/main/java/com/example/coroutines_lifecycle/MainActivity.kt
@@ -2,7 +2,14 @@ package com.example.coroutines_lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutines_lifecycle.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +20,22 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
+        }
+
+        lifecycleScope.launch(IO){
+            Log.d("Logd", "thread is ${Thread.currentThread().name}")
+        }
+
+        lifecycleScope.launchWhenCreated {
+
+        }
+
+        lifecycleScope.launchWhenStarted {
+
+        }
+
+        lifecycleScope.launchWhenResumed {
+
         }
     }
 }
--- Coroutines/Coroutines_Lifecycle/app/src/main/java/com/example/coroutines_lifecycle/ui/main/MainFragment.kt
+++ Coroutines/Coroutines_Lifecycle/app/src/main/java/com/example/coroutines_lifecycle/ui/main/MainFragment.kt
@@ -2,11 +2,15 @@ package com.example.coroutines_lifecycle.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutines_lifecycle.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
 
@@ -25,6 +29,10 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        lifecycleScope.launch(Dispatchers.IO){
+            Log.d("Logd", "thread is ${Thread.currentThread().name}")
+        }
     }
 
 }
