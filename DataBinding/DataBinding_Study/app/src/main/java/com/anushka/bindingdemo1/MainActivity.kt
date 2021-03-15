11652d3a1a8a496386410899b677340c1caae1cc
kimjw2003 <kimjw111003@gmail.com>
2021-03-15 15:44:57 +0900

    [STUDY] Room_Demo

--- DataBinding/DataBinding_Study/app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
+++ DataBinding/DataBinding_Study/app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.anushka.bindingdemo1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun displayGreeting() {
         binding.apply {
             greetingTextView.text = "Hello" + nameEditText.text
