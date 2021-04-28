c984549a8045256606147a64ba698f80fddbcb69
kimjw2003 <kimjw111003@gmail.com>
2021-04-28 12:08:07 +0900

    [Study] Working With Interface

--- Dagger/Working_With_Interface/app/src/main/java/com/anushka/didemo/Battery.kt
+++ Dagger/Working_With_Interface/app/src/main/java/com/anushka/didemo/Battery.kt
@@ -3,12 +3,7 @@ package com.anushka.didemo
 import android.util.Log
 import javax.inject.Inject
 
-class Battery @Inject constructor(){
-    init {
-        Log.i("MYTAG","Battery Constructed")
-    }
+interface Battery {
 
-    fun getPower(){
-        Log.i("MYTAG","Battery power is available")
-    }
+    fun getPower()
 }
--- Dagger/Working_With_Interface/app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++ Dagger/Working_With_Interface/app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
@@ -2,7 +2,7 @@ package com.anushka.didemo
 
 import dagger.Component
 
-@Component(modules = [MemoryCardModule::class])
+@Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
    fun getSmartPhone() : SmartPhone
 }
