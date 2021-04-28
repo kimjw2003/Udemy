3d24ff245d6c51c58e7845cd1fa287b1db15b11f
kimjw2003 <kimjw111003@gmail.com>
2021-04-28 12:20:03 +0900

    [Study] field Injection

--- Dagger/Dagger_Study/app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++ Dagger/Dagger_Study/app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
@@ -4,5 +4,6 @@ import dagger.Component
 
 @Component(modules = [MemoryCardModule::class])
 interface SmartPhoneComponent {
-    fun getSmartPhone() : SmartPhone
+
+    fun inject(mainActivity: MainActivity)
 }
