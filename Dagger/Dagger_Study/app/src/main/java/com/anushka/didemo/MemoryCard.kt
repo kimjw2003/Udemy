b7b62aa4b122d4fa61c0cbdf07489599a238e1bc
kimjw2003 <kimjw111003@gmail.com>
2021-04-28 11:06:45 +0900

    [Study] Dagger2 Modules

--- Dagger/Dagger_Study/app/src/main/java/com/anushka/didemo/MemoryCard.kt
+++ Dagger/Dagger_Study/app/src/main/java/com/anushka/didemo/MemoryCard.kt
@@ -3,7 +3,7 @@ package com.anushka.didemo
 import android.util.Log
 import javax.inject.Inject
 
-class MemoryCard @Inject constructor(){
+class MemoryCard{
     init {
         Log.i("MYTAG","Memory Card Constructed")
     }
